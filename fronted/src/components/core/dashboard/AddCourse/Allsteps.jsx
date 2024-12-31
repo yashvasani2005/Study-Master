b0a0966abbc8c0c97892e38f6e8a699b4d322b77
vasani Yash <vasaniyash2005@gmail.com>
2025-01-01 00:09:41 +0530

    create the succesfully component of Courseinformation

--- fronted/src/components/core/dashboard/AddCourse/Allsteps.jsx
+++ fronted/src/components/core/dashboard/AddCourse/Allsteps.jsx
@@ -1,48 +1,46 @@
 import { useSelector } from "react-redux";
 import { BiCheck } from "react-icons/bi";
 import CourseInfo from "./courseinformation/CourseInfo";
-import "./Allsteps.css"
+import CourseBuilder from "./courseBuilder/CourseBuilder";
+import "./Allsteps.css";
 
 export default function Allsteps() {
-  const {step}=useSelector((state)=>state.course)
+  const { step } = useSelector((state) => state.course);
 
-  const steps=[
+  const steps = [
     {
-        id:"1",
-        title:"Upload Course Information"
+      id: "1",
+      title: "Upload Course Information",
     },
     {
-        id:"2",
-        title:"Build The Course"
+      id: "2",
+      title: "Build The Course",
     },
     {
-        id:"3",
-        title:"Create The Course"
-    }
+      id: "3",
+      title: "Create The Course",
+    },
+  ];
 
-]
-    return (
-        <div className="mainbodyofallsteps">
-                   {
-                       <div>
-                             {steps.map((item)=>(
-                                <>
-                                  <div className={`${step===item.id ? "bg-color-yellow":"bg-richblack-500"}`}>
-                                    {
-                                        (step >item.id) ? (<BiCheck />) : (item.id)
-                                    }
-                                   <span className="deshing">-------------</span>
-                                   <div className="item_title">{item.title}</div>
+  return (
+    <div className="allsteps_container">
+      <div className="steps_list">
+        {steps.map((item) => (
+          <div
+            key={item.id}
+            className={`step_item ${
+              step >= item.id ? "bg-color-yellow" : "bg-richblack-500"
+            }`}
+          >
+            {step > item.id ? <BiCheck /> : item.id}
+            <span className="deshing">-------------</span>
+            <div className="item_title">{item.title}</div>
+          </div>
+        ))}
+      </div>
 
-                                  </div>
-                                </>
-                             ))}
-                        </div>
-                   }
-
-                   {step===1 && <CourseInfo/>}
-        </div>
-    );
-  }
-  
-  
+      {step === 1 && <CourseInfo />}
+      {step === 2 && <CourseBuilder />}
+    </div>
+  );
+}
---
+++ fronted/src/components/core/dashboard/AddCourse/courseBuilder/CourseBuilder.jsx
@@ -0,0 +1,11 @@
+import "./CourseBuilder.css"
+
+export default function CourseBuilder() {
+      
+
+  return (
+    <div className="Main_body_of_courseBuilder">
+    <h1>Hello Step 2  Apka swagat hain</h1>
+    </div>
+  );
+}
